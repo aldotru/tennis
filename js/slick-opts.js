33485c99a3e25fd4b4906934eb3e42d8f911f335
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-11-08 12:37:46 -0600

    Created slick carousel options file

---
+++ js/slick-opts.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+  // Rackets slick slider options
+  $('.rackets__slider').slick({
+    dots: true,
+    focusOnSelect: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    swipe: true,
+    swipeToSlide: true,
+    variableWidth: true,
+
+    // @type {object} DOM node
+    nextArrow: $('#slick__next'),
+    prevArrow: $('#slick__previous'),
+
+    // @type {object} DOM node
+    appendDots: $('.rackets__slider-dots')
+  });
+
+  $('.slick-carousel-3').slick({
+    
+  });
+  
+});
