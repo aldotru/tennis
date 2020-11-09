bd8e2b77c05bb8ce4034256ed93fbcfd1a67b9ae
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-11-08 18:32:07 -0600

    Added on-click functionality to display mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
-    
+    // Adding rotation effect to hamburguer icon
+    // and toggleing mobile menu
+    $('.hamburguer__icon').on('click', function() {
+
+        $(this).toggleClass('rotate');
+        $('.hamburguer__nav').toggleClass('show');
+
+    })
 
 });
